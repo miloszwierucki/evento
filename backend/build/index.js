c0f8bbae0a8b04dab46a9f13f310043af8ba6d15
miloszwierucki <milosz@wierucki.com>
2023-09-30 13:27:09 +0200

    Setup project

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,7 +1,7 @@
 import Fastify from "fastify";
 import { google } from "googleapis";
-import "dotenv/config";
 import fastifyStatic from "@fastify/static";
+import "dotenv/config";
 import * as path from "path";
 import { fileURLToPath } from "url";
 // Configuring the path to a root folder
@@ -14,9 +14,26 @@ const oauth2Client = new OAuth2(process.env.CLIENT_ID, process.env.SECRET_KEY);
 const calendar = google.calendar({
     version: "v3",
 });
+// Background color pallete
+var Color;
+(function (Color) {
+    Color[Color["gray"] = 0] = "gray";
+    Color[Color["blue"] = 1] = "blue";
+    Color[Color["red"] = 2] = "red";
+    Color[Color["pink"] = 3] = "pink";
+    Color[Color["grape"] = 4] = "grape";
+    Color[Color["violet"] = 5] = "violet";
+    Color[Color["indigo"] = 6] = "indigo";
+    Color[Color["cyan"] = 7] = "cyan";
+    Color[Color["teal"] = 8] = "teal";
+    Color[Color["green"] = 9] = "green";
+    Color[Color["lime"] = 10] = "lime";
+    Color[Color["yellow"] = 11] = "yellow";
+    Color[Color["orange"] = 12] = "orange";
+})(Color || (Color = {}));
 // Static website
 fastify.register(fastifyStatic, {
-    root: path.join(__dirname, "dist"),
+    root: path.join(__dirname, process.env.STATIC_FOLDER || "dist"),
 });
 // Insert event in Google calendar
 const postEventOpts = {
@@ -25,17 +42,21 @@ const postEventOpts = {
             type: "object",
             properties: {
                 accessToken: { type: "string" },
+                titleSchema: { type: "string" },
                 name: { type: "string" },
                 surname: { type: "string" },
                 description: { type: "string" },
+                guestEmail: { type: "string" },
                 startTime: { type: "string" },
                 duration: { type: "number" },
             },
             required: [
                 "accessToken",
+                "titleSchema",
                 "name",
                 "surname",
                 "description",
+                "guestEmail",
                 "startTime",
                 "duration",
             ],
@@ -43,7 +64,7 @@ const postEventOpts = {
     },
 };
 fastify.post("/insert-event/:uuid", postEventOpts, async (request, reply) => {
-    const { accessToken, name, surname, description, startTime, duration } = request.body;
+    const { accessToken, titleSchema, name, surname, description, guestEmail, startTime, duration, } = request.body;
     oauth2Client.setCredentials({
         access_token: accessToken,
     });
@@ -55,7 +76,9 @@ fastify.post("/insert-event/:uuid", postEventOpts, async (request, reply) => {
         sendUpdates: "all",
         auth: oauth2Client,
         requestBody: {
-            summary: "Konsultacja | " + name + " " + surname,
+            summary: titleSchema
+                .replace("${name}", name)
+                .replace("${surname}", surname),
             description: description + "\n\n#consultation",
             locked: true,
             start: {
@@ -66,7 +89,7 @@ fastify.post("/insert-event/:uuid", postEventOpts, async (request, reply) => {
                 dateTime: endEvent.toISOString(),
                 timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
             },
-            attendees: [{ email: "mily.deuce@gmail.com" }],
+            attendees: [{ email: guestEmail }],
             reminders: {
                 useDefault: false,
                 overrides: [
@@ -91,7 +114,7 @@ const getEventsOpts = {
         },
     },
 };
-fastify.put("/list-events/:uuid", (request, reply) => {
+fastify.put("/list-events/:uuid", getEventsOpts, (request, reply) => {
     const { accessToken } = request.body;
     oauth2Client.setCredentials({
         access_token: accessToken,
@@ -105,6 +128,8 @@ fastify.put("/list-events/:uuid", (request, reply) => {
         }
         else {
             const events = result?.data.items?.filter((e) => e.description?.includes("#consultation"));
+            events?.map((e) => (e.bgColor =
+                Color[Math.floor(Math.random() * 13)]));
             reply.send(events);
         }
     });
